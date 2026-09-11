90a5f65ade76a7311519eae93f9a56e1bfd889c0
XanPo-tech <xactineedschool@gmail.com>
2026-09-11 16:17:36 +0800

    ubah nama title lagi

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
+    <title>belajar php</title>
 </head>
 <body>
     <form action="index.php" method="post">
